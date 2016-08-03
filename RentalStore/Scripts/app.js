571defc97afc38eedd0efb9f840788ff0464ec6d
Yan Zaicev <danteivsinero@mail.ru>
2016-08-04 02:38:21 +0300

    two

--- RentalStore/Scripts/app.js
+++ RentalStore/Scripts/app.js
@@ -64,15 +64,14 @@
 
     run.$inject = ['$rootScope', '$location', '$cookieStore', '$http', "$window"];
     function run($rootScope, $location, $cookieStore, $http, $window) {
-        // keep user logged in after page refresh
+
         $rootScope.globals = $cookieStore.get('globals') || {};
         if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
         }
 
         $rootScope.$on('$locationChangeStart', function (event, next, current) {
-            console.log("asd");
-            // redirect to login page if not logged in and trying to access a restricted page
+
             var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
             var loggedIn = $rootScope.globals.currentUser;
             if (restrictedPage && !loggedIn) {
