85f0e01a4bc65b739c374b60b78b66e8f83ce73a
Yan Zaicev <danteivsinero@mail.ru>
2016-08-08 14:56:47 +0300

    one

--- RentalStore/Controllers/HomeController.cs
+++ RentalStore/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace RentalStore.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+
         public ActionResult Index()
         {
             return View();
